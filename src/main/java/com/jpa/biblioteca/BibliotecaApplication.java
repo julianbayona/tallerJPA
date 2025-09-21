b1b413d59f8889c2bf563e76cecb3c45167385e1
julianbayona <julianbayona0315@gmail.com>
2025-09-21 17:06:47 -0500

    first commit

---
+++ src/main/java/com/jpa/biblioteca/BibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaApplication.class, args);
+	}
+
+}
